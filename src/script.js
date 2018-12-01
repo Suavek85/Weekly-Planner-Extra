8a6596767e2e2b0f4a647f15900f73b6ba286176
SNiemiec <slawekniemiec@gmail.com>
2018-12-01 08:05:24 +0100

    API response sorted

--- src/script.js
+++ src/script.js
@@ -12,6 +12,7 @@ import {
 
 
 //LOCATION AND WEATHER
+var superArray = [];
 
 
 function geoFindMe() {
@@ -36,21 +37,32 @@ function geoFindMe() {
         return data.json()
       })
       .then(res => {
-        document.getElementById('weather').innerHTML = `${res.main.temp}° in ${res.name}`;
-        console.log(res);
+        document.getElementById('weather').innerHTML = `${res.main.temp}° in`;
+        document.getElementById('weather-city').innerHTML = `${res.name}`;
+       console.log(res);
 
 document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/${res.weather[0].icon}.png`
 
       })
 
 
+
     fetch(url2)
     .then(data => {
       return data.json()
     })
+    
+    
     .then(res => {
       
       console.log(res);
+      //superArray.push(res);
+      console.log(res.list[0], res.list[8], res.list[16], res.list[24], res.list[32],res.list[39],res.list[39]);
+
+      //firstDayWeather = function() { return res.list[0].main.temp + ' in ' + res.city.name; }
+      //firstDayWeather =  res.list[0].main.temp + ' in ' + res.city.name; 
+      //`${res.list[0].main.temp}° in ${res.name}`
+      //http://openweathermap.org/img/w/${res.list[0].weather[0].icon}.png
 
     })
 
@@ -70,7 +82,7 @@ document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/$
         return data.json()
       })
       .then(res => {
-        document.getElementById('weather').innerHTML = `${res.main.temp}° in ${res.name}`;
+        document.getElementById('weather').innerHTML = `${res.main.temp_min} - ${res.main.temp_min}° in ${res.name}`;
         document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/${res.weather[0].icon}.png`
         console.log(res);
       })
@@ -94,6 +106,80 @@ document.getElementById('weather-icon').src = `http://openweathermap.org/img/w/$
 
   navigator.geolocation.getCurrentPosition(success, error);
 }
+
+
+
+
+function zaba(arg) {
+
+  var yourCity = document.getElementById('weather-city').innerHTML;
+  console.log(yourCity);
+  var apiKey2 = '44bec28a349022ede2335eaf35ab8433';
+  var url2 = `http://api.openweathermap.org/data/2.5/forecast?q=${yourCity}&units=metric&appid=${apiKey2}`;
+  
+  
+  fetch(url2)
+      .then(data => {
+        return data.json()
+      })
+      
+      
+      .then(res => {
+        
+        console.log(res);
+        console.log(Math.round(res.list[0].main.temp_min) + '-' + Math.round(res.list[0].main.temp_max) + '° in ' + res.city.name) 
+        console.log(`http://openweathermap.org/img/w/${res.list[0].weather[0].icon}.png`)
+        console.log(res.list[0].dt, res.list[8].dt, res.list[16].dt, res.list[24].dt)
+        
+    
+    var tomb = [res.list[0].dt, res.list[8].dt, res.list[16].dt, res.list[24].dt, res.list[32].dt, res.list[33].dt, res.list[34].dt ] ;
+tomb.forEach(function(el, index) {
+
+  
+var anotherNo;
+
+  console.log(index);
+  
+  var xx = new Date();
+  xx.setTime(el*1000);
+  console.log(xx.toUTCString());
+  console.log(xx.getDay())
+
+  if (xx.getDay() ===  arg) {
+
+if(index === 0) { anotherNo = 0 } 
+else if ( index === 1) { anotherNo = 8 }
+else if ( index === 2) { anotherNo = 16 }
+else if ( index === 3) { anotherNo = 24 }
+else if ( index === 4) { anotherNo = 32 }
+else if ( index === 5) { anotherNo = 38 }
+else if ( index === 6) { anotherNo = 39 }
+else { anotherNo = 0 } 
+
+    document.getElementById('card-weather-id').innerHTML =  Math.round(res.list[anotherNo].main.temp_max) + '° in ' + res.city.name;
+    
+    document.getElementById('card-weather-icon').src = `http://openweathermap.org/img/w/${res.list[anotherNo].weather[0].icon}.png`
+
+
+  } else{ console.log("Nope")}
+
+
+
+})
+    
+
+  //})
+
+      }
+  
+  
+      )}
+
+    //var timestamp = res.list[0].dt; // UNIX timestamp in seconds
+    //var xx = new Date();
+    //xx.setTime(timestamp*1000); // javascript timestamps are in milliseconds
+    //console.log(xx.toUTCString());
+    //console.log(xx.getDay())
 
 
 
@@ -183,46 +269,68 @@ document.addEventListener(
       });
 
       if (eventbtn_open === "btns_1_open") {
+
+        zaba(1);
         if (dates.notToday(1)) {
           dates.jumpToNextDay(dates.date(), 6);
         } else {
           dates.jumpToToday();
+          
         }
       } else if (eventbtn_open === "btns_2_open") {
+
+        zaba(2);
         if (dates.notToday(2)) {
           dates.jumpToNextDay(dates.date(), 5);
         } else {
           dates.jumpToToday();
+          
         }
       } else if (eventbtn_open === "btns_3_open") {
+
+        zaba(3);
         if (dates.notToday(3)) {
           dates.jumpToNextDay(dates.date(), 4);
         } else {
           dates.jumpToToday();
+          
         }
       } else if (eventbtn_open === "btns_4_open") {
+
+        zaba(4);
         if (dates.notToday(4)) {
           dates.jumpToNextDay(dates.date(), 3);
         } else {
           dates.jumpToToday();
+          
+
         }
       } else if (eventbtn_open === "btns_5_open") {
+
+        zaba(5);
         if (dates.notToday(5)) {
           dates.jumpToNextDay(dates.date(), 2);
         } else {
           dates.jumpToToday();
+          
         }
       } else if (eventbtn_open === "btns_6_open") {
+
+        zaba(6);
         if (dates.notToday(6)) {
           dates.jumpToNextDay(dates.date(), 1);
         } else {
           dates.jumpToToday();
+          
         }
       } else if (eventbtn_open === "btns_7_open") {
+
+        zaba(0);
         if (dates.notToday(7)) {
           dates.jumpToNextDay(dates.date(), 0);
         } else {
           dates.jumpToToday();
+          
         }
       } else {
         return;
